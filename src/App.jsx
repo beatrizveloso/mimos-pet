f1dca1c807ed84cbba23663239162c1b1d06057d
bibsveloso <beatrizoslov6@gmail.com>
2025-03-20 00:28:29 -0300

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/Header.jsx';
+import Home from './components/home/Home.jsx';
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 };
