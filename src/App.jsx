d28c35b8e53b30b469a1079392eba171aef252fe
bibsveloso <beatrizoslov6@gmail.com>
2025-03-21 22:40:03 -0300

    text-products

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/Header.jsx';
 import Home from './components/home/Home.jsx';
+import Produtos from './components/produtos/Produtos.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <Produtos />
     </>
   );
 };
---
+++ src/components/produtos/Produtos.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaArrowRight } from "react-icons/fa"; 
+import { FiArrowRight } from "react-icons/fi"; 
+
+import "../produtos/Produtos.css";
+
+const Produtos = () => {
+    return (
+        <div className="align-products">
+            <div className="products-text">
+                <p>Explore nossa seleção e encontre tudo para o seu pet</p>
+                <h1>Nossos produtos:</h1>
+            </div>
+            <div className="products-button">
+                <button className="custom-button">
+                    <p>Ver mais</p> 
+                    <FiArrowRight className="arrow-icon" />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Produtos;
