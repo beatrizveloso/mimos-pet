1e92e4481ed551081430ac0719a4c88a8c8726ca
bibsveloso <beatrizoslov6@gmail.com>
2025-03-21 21:12:30 -0300

    animation home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -12,9 +12,8 @@ const Home = () => {
             <h1>Mimo’s <span>pet</span></h1>
             <img src="images/retangulinho.svg" alt="" />
             </div>
-            <p>Aqui você encontra tudo para cuidar do seu pet <br /> com carinho e praticidade. Produtos de <br />qualidade, entrega rápida e muito amor em <br />cada escolha. Tudo o que seu pet precisa, a um <br />clique de distância!</p>
+            <p>Aqui você encontra tudo para cuidar do seu pet  com carinho e praticidade. Produtos de qualidade, entrega rápida e muito amor em cada escolha. Tudo o que seu pet precisa, a um clique de distância!</p>
             <button>Ver Produtos</button>
-          
         </div>
         </main>
     );
