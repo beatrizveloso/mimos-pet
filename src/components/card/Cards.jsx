8081ab4edc15157ebc762e5b17f8c0cbd465cbef
beatrizveloso <beatrizoslov6@gmail.com>
2025-03-27 23:23:16 -0300

    adicionei cards de ração

--- src/components/card/Cards.jsx
+++ src/components/card/Cards.jsx
@@ -4,7 +4,7 @@ import Card from './Card';
 
 const Cards = ({ filtro, searchTerm, limit, paginated }) => {
   const produtosData = [
-    { image: 'images/ração.png', title: 'Ração Seca Para Gatos Adultos Carne', category: 'Alimentação', size: 'Tamanho: 2,7kg', price: 'R$42,42' },
+    { image: 'images/ração-1.png', title: 'Ração Seca Para Gatos Adultos Carne', category: 'Alimentação', size: 'Tamanho: 2,7kg', price: 'R$42,42' },
     { image: 'images/pipicat.png', title: 'Areia Higiênica Pipicat Classic Para Gatos', category: 'Higiene', size: 'Tamanho: 4kg', price: 'R$12,25' },
     { image: 'images/mochila-cat.png', title: 'Transporte Mochila Pet Gatos E Cães', category: 'Transporte', size: 'Tamanho: Único', price: 'R$347,99' },
     { image: 'images/bola-cravo.png', title: 'Brinquedo Bola Vinil com Cravo para Cães', category: 'Brinquedo', size: 'Tamanho: P', price: 'R$14,49' },
@@ -12,14 +12,23 @@ const Cards = ({ filtro, searchTerm, limit, paginated }) => {
     { image: 'images/comedouro.png', title: 'Comedouro E Brinquedo Petball Mini Pet Games', category: 'Brinquedo', size: 'Tamanho: P', price: 'R$36,46' },
     { image: 'images/shampoo-neutro.png', title: 'Shampoo Pet Clean Neutro para Cães e Gatos', category: 'Higiene', size: 'Tamanho: 700ml', price: 'R$17,90' },
     { image: 'images/frango-galinha.png', title: 'Frango Galinha Borracha Morder Brinquedo Pet', category: 'Brinquedo', size: 'Tamanho: Único', price: 'R$19,60' },
-    { image: 'images/ração.png', title: 'Ração Seca Para Gatos Adultos Carne', category: 'Alimentação', size: 'Tamanho: 2,7kg', price: 'R$42,42' },
-    { image: 'images/pipicat.png', title: 'Areia Higiênica Pipicat Classic Para Gatos', category: 'Higiene', size: 'Tamanho: 4kg', price: 'R$12,25' },
-    { image: 'images/mochila-cat.png', title: 'Transporte Mochila Pet Gatos E Cães', category: 'Transporte', size: 'Tamanho: Único', price: 'R$347,99' },
-    { image: 'images/bola-cravo.png', title: 'Brinquedo Bola Vinil com Cravo para Cães', category: 'Brinquedo', size: 'Tamanho: P', price: 'R$14,49' },
-    { image: 'images/brinquedo-ventosa.png', title: 'Brinquedo Corda Ventosa Morder Pet Cachorro Azul', category: 'Brinquedo', size: 'Tamanho: Único', price: 'R$19,30' },
-    { image: 'images/comedouro.png', title: 'Comedouro E Brinquedo Petball Mini Pet Games', category: 'Brinquedo', size: 'Tamanho: P', price: 'R$36,46' },
-    { image: 'images/shampoo-neutro.png', title: 'Shampoo Pet Clean Neutro para Cães e Gatos', category: 'Higiene', size: 'Tamanho: 700ml', price: 'R$17,90' },
-    { image: 'images/frango-galinha.png', title: 'Frango Galinha Borracha Morder Brinquedo Pet', category: 'Brinquedo', size: 'Tamanho: Único', price: 'R$19,60' },
+    
+    { image: 'images/ração-2.png', title: 'Ração Golden Frango e Carne Gatos castrados', category: 'Alimentação', size: 'Tamanho: 10,1kg', price: 'R$141,21' },
+    { image: 'images/ração-3.png', title: 'Ração Special Cat Premium Para Gatos Adultos Castrados', category: 'Alimentação', size: 'Tamanho: 1kg', price: 'R$24,90' },
+    { image: 'images/ração-4.png', title: 'Gran Plus Ração Gatos Filhotes Frango e Arroz', category: 'Alimentação', size: 'Tamanho: 1kg', price: 'R$27,90' },
+    { image: 'images/ração-5.png', title: 'Ração para gatos Filhotes  Frango Leite Cenoura', category: 'Alimentação', size: 'Tamanho: 1kg', price: 'R$29,90' },
+    { image: 'images/ração-6.png', title: 'Ração Spert Cat Premium para Gatos Frango e Salmão', category: 'Alimentação', size: 'Tamanho: 10,1kg', price: 'R$149,84' },
+    { image: 'images/ração-7.png', title: 'Ração Golden Gatos Castrados Frango com Abóbora', category: 'Alimentação', size: 'Tamanho: 3kg', price: 'R$73,90' },
+    { image: 'images/ração-8.png', title: 'Ração Golden Gatos Castrados Salmão Premium', category: 'Alimentação', size: 'Tamanho: 10,1kg', price: 'R$167,10' },
+    { image: 'images/ração-9.png', title: 'Mandala Blend Gatos Adultos Carne e Frango ', category: 'Alimentação', size: 'Tamanho: 10,1kg', price: 'R$109,90' },
+    { image: 'images/ração-10.png', title: 'Ração Gatos Castrados Salmão e Batata doce', category: 'Alimentação', size: 'Tamanho: 10,1kg', price: 'R$184,89' },
+    { image: 'images/ração-11.png', title: 'Ração Premier Gatos Adultos Castrados Frango ', category: 'Alimentação', size: 'Tamanho: 1,5kg', price: 'R$68,57' },
+    { image: 'images/ração-12.png', title: 'Ração Gatos Castrados Super Premium Pet Gold ', category: 'Alimentação', size: 'Tamanho: 10,1kg', price: 'R$124,29' },
+    { image: 'images/ração-13.png', title: 'Ração Magnus Mix Partículas Recheadas para Gatos Adultos ', category: 'Alimentação', size: 'Tamanho: 25kg', price: 'R$259,66' },
+    { image: 'images/ração-14.png', title: 'Ração Magnus Cat Premium Gatos Filhotes Mix de Sabores', category: 'Alimentação', size: 'Tamanho: 15kg', price: 'R$162,14' },
+    { image: 'images/ração-15.png', title: 'Ração Special Cat Gatos Adultos Carne', category: 'Alimentação', size: 'Tamanho: 20kg', price: 'R$238,90' },
+    { image: 'images/ração-16.png', title: 'Magnus Cat Premium Gatos Filhotes Carne E Frango', category: 'Alimentação', size: 'Tamanho: 10kg', price: 'R$162,12' },
+    
   ];
 
   const [paginaAtual, setPaginaAtual] = useState(1);
--- src/pages/loginpage/LoginPage.jsx
+++ src/pages/loginpage/LoginPage.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom"; 
+import { FaArrowLeft } from "react-icons/fa";
 import "./LoginPage.css";
 
 const LoginPage = () => {
@@ -12,84 +14,109 @@ const LoginPage = () => {
 
   return (
     <section className="bg-login">
-    <div className={`login container grid ${isRegistering ? "active" : ""}`} id="loginAccessRegister">
-      <svg className="login__blob" viewBox="0 0 566 840" xmlns="http://www.w3.org/2000/svg">
-        <mask id="mask0" maskType="alpha">
-          <path d="M342.407 73.6315C388.53 56.4007 394.378 17.3643 391.538 0H566V840H0C14.5385 834.991 100.266 804.436 77.2046 707.263C49.6393 591.11 115.306 518.927 176.468 488.873C363.385 397.026 156.98 302.824 167.945 179.32C173.46 117.209 284.755 95.1699 342.407 73.6315Z"/>
-        </mask>
-        <g mask="url(#mask0)">
-          <path d="M342.407 73.6315C388.53 56.4007 394.378 17.3643 391.538 0H566V840H0C14.5385 834.991 100.266 804.436 77.2046 707.263C49.6393 591.11 115.306 518.927 176.468 488.873C363.385 397.026 156.98 302.824 167.945 179.32C173.46 117.209 284.755 95.1699 342.407 73.6315Z"/>
-          <image className="login__img" href="/images/bg-img2.jpg" />
-        </g>
-      </svg>
-      
-      {/* Login Section */}
-      <div className="login__access">
-        <h1 className="login__title">Entre na sua conta.</h1>
-        <div className="login__area">
-          <div className="login__content grid">
-            <div className="login__box">
-              <input type="text" id="email" placeholder=" " className="login__input" />
-              <label htmlFor="email" className="login__label">Email</label>
-              <i className="ri-mail-fill login__icon"></i>
+
+      <Link to="/">
+        <div className="back-arrow-container">
+          <button className="back-btn">
+            <FaArrowLeft className="back-icon" />
+          </button>
+        </div>
+      </Link>
+
+      <div className={`login container grid ${isRegistering ? "active" : ""}`} id="loginAccessRegister">
+        <svg className="login__blob" viewBox="0 0 566 840" xmlns="http://www.w3.org/2000/svg">
+          <mask id="mask0" maskType="alpha">
+            <path d="M342.407 73.6315C388.53 56.4007 394.378 17.3643 391.538 0H566V840H0C14.5385 834.991 100.266 804.436 77.2046 707.263C49.6393 591.11 115.306 518.927 176.468 488.873C363.385 397.026 156.98 302.824 167.945 179.32C173.46 117.209 284.755 95.1699 342.407 73.6315Z" />
+          </mask>
+          <g mask="url(#mask0)">
+            <path d="M342.407 73.6315C388.53 56.4007 394.378 17.3643 391.538 0H566V840H0C14.5385 834.991 100.266 804.436 77.2046 707.263C49.6393 591.11 115.306 518.927 176.468 488.873C363.385 397.026 156.98 302.824 167.945 179.32C173.46 117.209 284.755 95.1699 342.407 73.6315Z" />
+            <image className="login__img" href="/images/bg-img2.jpg" />
+          </g>
+        </svg>
+
+        <div className="login__access">
+          <h1 className="login__title">Entre na sua conta.</h1>
+          <div className="login__area">
+            <div className="login__content grid">
+              <div className="login__box">
+                <input type="text" id="email" placeholder=" " className="login__input" />
+                <label htmlFor="email" className="login__label">Email</label>
+                <i className="ri-mail-fill login__icon"></i>
+              </div>
+              <div className="login__box">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  placeholder=" "
+                  className="login__input"
+                />
+                <label htmlFor="password" className="login__label">Senha</label>
+                <i
+                  className={`ri-eye${showPassword ? "-off" : ""}-fill login__icon login__password`}
+                  onClick={() => setShowPassword(!showPassword)}
+                ></i>
+              </div>
             </div>
-            <div className="login__box">
-              <input 
-                type={showPassword ? "text" : "password"} 
-                id="password" 
-                placeholder=" " 
-                className="login__input" 
-              />
-              <label htmlFor="password" className="login__label">Senha</label>
-              <i 
-                className={`ri-eye${showPassword ? "-off" : ""}-fill login__icon login__password`} 
-                onClick={() => setShowPassword(!showPassword)}
-              ></i>
+            <a href="#" className="login__forgot">Esqueceu sua senha?</a>
+            <button className="login__button" onClick={() => alert("Login bem-sucedido!")}>Login</button>
+
+            <div className="login__social">
+              <p className="login__social-title">Ou faça login com</p>
+              <div className="login__social-links">
+                <a href="#" className="login__social-link">
+                  <img src="/images/icon-google.svg" alt="Google" className="login__social-img" />
+                </a>
+                <a href="#" className="login__social-link">
+                  <img src="/images/icon-facebook.svg" alt="Facebook" className="login__social-img" />
+                </a>
+                <a href="#" className="login__social-link">
+                  <img src="/images/icon-apple.svg" alt="Apple" className="login__social-img" />
+                </a>
+              </div>
             </div>
+
+
+            <p className="login__switch">Não possui uma conta?
+              <button onClick={() => setIsRegistering(true)}>Crie uma conta</button>
+            </p>
           </div>
-          <a href="#" className="login__forgot">Esqueceu sua senha?</a>
-          <button className="login__button" onClick={() => alert("Login bem-sucedido!")}>Login</button>
-          <p className="login__switch">Não possui uma conta? 
-            <button onClick={() => setIsRegistering(true)}>Crie uma conta</button>
-          </p>
+        </div>
+
+        <div className="login__register">
+          <h1 className="login__title">Criar nova conta.</h1>
+          <div className="login__area">
+            <div className="login__content grid">
+              <div className="login__box">
+                <input type="text" id="names" placeholder=" " className="login__input" />
+                <label htmlFor="names" className="login__label">Nome</label>
+                <i className="ri-id-card-fill login__icon"></i>
+              </div>
+              <div className="login__box">
+                <input type="text" id="emailCreate" placeholder=" " className="login__input" />
+                <label htmlFor="emailCreate" className="login__label">Email</label>
+                <i className="ri-mail-fill login__icon"></i>
+              </div>
+              <div className="login__box">
+                <input
+                  type={showRegisterPassword ? "text" : "password"}
+                  id="passwordCreate"
+                  placeholder=" "
+                  className="login__input"
+                />
+                <label htmlFor="passwordCreate" className="login__label">Senha</label>
+                <i
+                  className={`ri-eye${showRegisterPassword ? "-off" : ""}-fill login__icon login__password`}
+                  onClick={() => setShowRegisterPassword(!showRegisterPassword)}
+                ></i>
+              </div>
+            </div>
+            <button className="login__button" onClick={() => alert("Conta criada com sucesso!")}>Criar conta</button>
+            <p className="login__switch">Já possui uma conta?
+              <button onClick={() => setIsRegistering(false)}>Log In</button>
+            </p>
+          </div>
         </div>
       </div>
-      
-      <div className="login__register">
-        <h1 className="login__title">Criar nova conta.</h1>
-        <div className="login__area">
-          <div className="login__content grid">
-            <div className="login__box">
-              <input type="text" id="names" placeholder=" " className="login__input" />
-              <label htmlFor="names" className="login__label">Nome</label>
-              <i className="ri-id-card-fill login__icon"></i>
-            </div>
-            <div className="login__box">
-              <input type="text" id="emailCreate" placeholder=" " className="login__input" />
-              <label htmlFor="emailCreate" className="login__label">Email</label>
-              <i className="ri-mail-fill login__icon"></i>
-            </div>
-            <div className="login__box">
-              <input 
-                type={showRegisterPassword ? "text" : "password"} 
-                id="passwordCreate" 
-                placeholder=" " 
-                className="login__input" 
-              />
-              <label htmlFor="passwordCreate" className="login__label">Senha</label>
-              <i 
-                className={`ri-eye${showRegisterPassword ? "-off" : ""}-fill login__icon login__password`} 
-                onClick={() => setShowRegisterPassword(!showRegisterPassword)}
-              ></i>
-            </div>
-          </div>
-          <button className="login__button" onClick={() => alert("Conta criada com sucesso!")}>Criar conta</button>
-          <p className="login__switch">Já possui uma conta? 
-            <button onClick={() => setIsRegistering(false)}>Log In</button>
-          </p>
-        </div>
-      </div>
-    </div>
     </section>
   );
 };
