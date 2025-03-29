d57e153ffa207b1fb92d177605f1af5b28ecc660
beatrizveloso <beatrizoslov6@gmail.com>
2025-03-29 01:20:46 -0300

    zoom image

--- src/components/productsdetail/ProductDetail.jsx
+++ src/components/productsdetail/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { FaStar, FaMinus, FaPlus } from 'react-icons/fa';
 import './ProductDetail.css';
@@ -41,20 +41,48 @@ const produtosData = [
 
 const ProductDetail = () => {
   const { id } = useParams();
-  const navigate = useNavigate();
   const produto = produtosData.find(prod => prod.id === parseInt(id));
   const [quantidade, setQuantidade] = useState(1);
+
+  const [zoomPos, setZoomPos] = useState({ x: 0, y: 0, visible: false });
+  const imgRef = useRef(null);
+  const zoom = 2.5; // Fator de zoom
 
   if (!produto) {
     return <h1>Produto não encontrado</h1>;
   }
 
+  const handleMouseMove = (e) => {
+    const img = imgRef.current;
+    const { left, top, width, height } = img.getBoundingClientRect();
+    const x = ((e.clientX - left) / width) * 100;
+    const y = ((e.clientY - top) / height) * 100;
+
+    setZoomPos({ x, y, visible: true });
+  };
+
+
   return (
     <div className="product-detail-container">
       <div className="product-detail">
 
-        <div className="image-and-info">
-          <img src={produto.image} alt={produto.title} className="product-image" />
+      <div 
+          className="image-container"
+          onMouseMove={handleMouseMove}
+          onMouseLeave={() => setZoomPos({ ...zoomPos, visible: false })}
+        >
+          <img ref={imgRef} src={produto.image} alt={produto.title} className="product-image" />
+
+          {zoomPos.visible && (
+            <div
+              className="zoom-box"
+              style={{
+                backgroundImage: `url(${produto.image})`,
+                backgroundPosition: `${zoomPos.x}% ${zoomPos.y}%`,
+                backgroundSize: `${zoom * 100}%`
+              }}
+            />
+          )}
         </div>
 
         <div className="product-info">
